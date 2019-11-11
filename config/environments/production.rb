3819e4343fdd868733cfaf2cb4bff5e7cb8cb623
David Kilzer <kilzer15@gmail.com>
2019-11-10 23:00:25 -0600

    Adding Mailer to production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,27 @@
 Rails.application.configure do
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 5000 }
+  config.action_mailer.default :charset => "utf-8"
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+  address:              "smtp.gmail.com",
+  port:                 587,
+  user_name:            "handsoflyfe@gmail.com",
+  password:             "Lovemassage2018",
+  authentication:       "plain",
+  enable_starttls_auto: true
+}
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.perform_caching = false
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
